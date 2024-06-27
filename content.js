3feb93e6ba28719ad1fa9b994db9200e54595627
prslv <presli75@gmail.com>
2024-06-27 13:24:11 +0300

    license & misc

--- content.js
+++ content.js
@@ -1,4 +1,31 @@
+/*
+MIT License with Additional Restrictions and Disclaimer
+
+Copyright (c) 2024 Preslav Kunov, repuddle.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+1. The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+2. The Software, or any derivative works thereof, may not be used for commercial purposes, including but not limited to the sale of the Software, integration into commercial products, or for any use that generates monetary gain.
+
+3. The Software, or any derivative works thereof, may not be patented or used in any way that infringes on the intellectual property rights of the original author.
+
+4. Any modifications made to the Software must be clearly indicated, and the original copyright notice and permission notice shall be retained in the derivative works.
+
+5. Redistributions of the Software, modified or unmodified, must reproduce the above copyright notice, this list of conditions, and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 let overlayDisplayed = false;
+
 chrome.storage.local.get(['focusMode', 'blockedSites', 'blockMode'], (data) => {
     const { focusMode, blockedSites, blockMode } = data;
     if (focusMode && blockedSites) {
@@ -13,7 +40,6 @@ chrome.storage.local.get(['focusMode', 'blockedSites', 'blockMode'], (data) => {
     }
 });
 
-console.log('CONTENT JS INJECTED');
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'toggleFocusMode') {
         console.log('HELLO');
@@ -65,6 +91,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         location.reload(); // Reload the tab if overlay is displayed
     }
 });
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'applyBlockMode') {
         const mode = message.mode;
@@ -80,6 +107,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         });
     }
 });
+
 function showOverlay(mode) {
     console.log(mode);
     switch (mode) {
@@ -93,6 +121,7 @@ function showOverlay(mode) {
             console.error('Invalid block mode');
     }
 }
+
 function displayOverlay() {
     if (!overlayDisplayed) {
         overlayDisplayed = true;
@@ -124,6 +153,7 @@ function displayOverlay() {
         }
     }
 }
+
 function closeTabs() {
     chrome.runtime.sendMessage({ action: 'closeCurrentTab' }, function (response) {
         if (response && response.success) {
@@ -133,6 +163,7 @@ function closeTabs() {
         }
     });
 }
+
 function hideOverlay() {
     if (overlayDisplayed) {
         overlayDisplayed = false;
--- popup.js
+++ popup.js
@@ -1,3 +1,28 @@
+/*
+MIT License with Additional Restrictions and Disclaimer
+
+Copyright (c) 2024 Preslav Kunov, repuddle.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+1. The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+2. The Software, or any derivative works thereof, may not be used for commercial purposes, including but not limited to the sale of the Software, integration into commercial products, or for any use that generates monetary gain.
+
+3. The Software, or any derivative works thereof, may not be patented or used in any way that infringes on the intellectual property rights of the original author.
+
+4. Any modifications made to the Software must be clearly indicated, and the original copyright notice and permission notice shall be retained in the derivative works.
+
+5. Redistributions of the Software, modified or unmodified, must reproduce the above copyright notice, this list of conditions, and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 document.addEventListener('DOMContentLoaded', () => {
     const toggleFocusButton = document.getElementById('toggleFocus');
     const addSiteButton = document.getElementById('addSite');
@@ -22,7 +47,9 @@ document.addEventListener('DOMContentLoaded', () => {
         // console.log(data.focusMode);
         // console.log(data.blockedSites);
     });
+    //
 
+    //EVENT LISTENERS
     siteInput.addEventListener('input', () => {
         if (siteInput.value.trim() !== '') {
             addSiteButton.classList.remove('hidden');
@@ -40,32 +67,6 @@ document.addEventListener('DOMContentLoaded', () => {
     addSiteButton.addEventListener('click', () => {
         handleInputUrls(siteInput, addSiteButton);
     });
-
-    function handleInputUrls(input, addbtn) {
-        const newSite = input.value.trim();
-        if (newSite) {
-            addNewSite(newSite);
-            addbtn.classList.add('hidden');
-
-            chrome.storage.local.get(['focusMode', 'blockMode'], (data) => {
-                const newFocusMode = data.focusMode;
-                const blockMode = data.blockMode;
-
-                if (newFocusMode) {
-                    chrome.tabs.query({}, (tabs) => {
-                        tabs.forEach((tab) => {
-                            chrome.tabs.sendMessage(tab.id, { action: "addUrl", mode: blockMode, focusMode: newFocusMode, site: newSite }, (response) => {
-                                if (chrome.runtime.lastError) {
-                                    // console.error('Error sending message: ' + chrome.runtime.lastError.message);
-                                }
-                            });
-                        });
-                    });
-                }
-            });
-        }
-        siteInput.value = '';
-    }
 
     toggleFocusButton.addEventListener('click', () => {
         chrome.storage.local.get(['focusMode', 'blockMode'], (data) => {
@@ -120,18 +121,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    function addNewSite(newSite) {
-        chrome.storage.local.get('blockedSites', (data) => {
-            const blockedSites = data.blockedSites || [];
-            if (!blockedSites.includes(newSite)) {
-                blockedSites.push(newSite);
-                chrome.storage.local.set({ blockedSites: blockedSites }, () => {
-                    updateUI(null, blockedSites);
-                });
-            }
-        });
-    }
-
     blockedSitesList.addEventListener('click', (event) => {
         if (event.target.classList.contains('remove-site')) {
             const siteToRemove = event.target.dataset.site;
@@ -161,11 +150,12 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+    //
 
+    // SET BLOCKING MODE (CLOSE TABS / DISPLAY OVERLAY)
     const mode1Radio = document.getElementById('mode1Radio');
     const mode2Radio = document.getElementById('mode2Radio');
 
-    // Initialize radio buttons based on stored value
     chrome.storage.local.get('blockMode', (data) => {
         if (data.blockMode === 2) {
             mode2Radio.checked = true;
@@ -174,26 +164,60 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Event listener for Mode 1 radio
     mode1Radio.addEventListener('change', () => {
         if (mode1Radio.checked) {
             chrome.storage.local.set({ blockMode: 1 });
             applyBlockMode(1);
-            console.log('mode 1 checked');
+            // console.log('mode 1 checked');
         }
     });
 
-    // Event listener for Mode 2 radio
     mode2Radio.addEventListener('change', () => {
         if (mode2Radio.checked) {
             chrome.storage.local.set({ blockMode: 2 });
             applyBlockMode(2);
-            console.log('mode 2 checked');
+            // console.log('mode 2 checked');
         }
     });
+    //
 
+    // FUNCTIONS
+    function handleInputUrls(input, addbtn) {
+        const newSite = input.value.trim();
+        if (newSite) {
+            addNewSite(newSite);
+            addbtn.classList.add('hidden');
 
-    // Function to apply block mode
+            chrome.storage.local.get(['focusMode', 'blockMode'], (data) => {
+                const newFocusMode = data.focusMode;
+                const blockMode = data.blockMode;
+
+                if (newFocusMode) {
+                    chrome.tabs.query({}, (tabs) => {
+                        tabs.forEach((tab) => {
+                            chrome.tabs.sendMessage(tab.id, { action: "addUrl", mode: blockMode, focusMode: newFocusMode, site: newSite }, (response) => {
+                                if (chrome.runtime.lastError) {
+                                    // console.error('Error sending message: ' + chrome.runtime.lastError.message);
+                                }
+                            });
+                        });
+                    });
+                }
+            });
+        }
+        siteInput.value = '';
+    }
+    function addNewSite(newSite) {
+        chrome.storage.local.get('blockedSites', (data) => {
+            const blockedSites = data.blockedSites || [];
+            if (!blockedSites.includes(newSite)) {
+                blockedSites.push(newSite);
+                chrome.storage.local.set({ blockedSites: blockedSites }, () => {
+                    updateUI(null, blockedSites);
+                });
+            }
+        });
+    }
     function applyBlockMode(mode) {
         chrome.storage.local.get('focusMode', (data) => {
             const newFocusMode = data.focusMode;
@@ -208,8 +232,6 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     }
-
-
     function updateUI(focusMode, blockedSites) {
         const toggleFocusButton = document.getElementById('toggleFocus');
         if (focusMode !== null) {
@@ -250,6 +272,7 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
     }
+    //
 
     // function shouldApplyFocusMode(hostname, focusMode, blockedSitesList) {
     //     // Check if the hostname is in the blockedSitesList
@@ -258,6 +281,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // }
 });
 
+// EXPAND BUTTONS
 const supBtn = document.getElementById("expand-btn");
 const supDiv = document.querySelector(".expand-div");
 
@@ -308,8 +332,57 @@ setBtn.addEventListener("click", () => {
         setBtn.classList.remove("expanded");
     }
 });
+//
 
-// ADD STARS SCRIPT BELOW
+// STARS RATING
+const stars = document.querySelectorAll('.star');
+function setRating(value) {
+    localStorage.setItem('userRating', value);
+}
 
+function getRating() {
+    return localStorage.getItem('userRating');
+}
 
-////////////
+function updateStars() {
+    const rating = getRating();
+    if (rating) {
+        const value = parseInt(rating);
+        stars.forEach((star, index) => {
+            if (index < value) {
+                star.classList.add('selected');
+            } else {
+                star.classList.remove('selected');
+            }
+        });
+    }
+}
+
+updateStars();
+
+stars.forEach((star) => {
+    star.addEventListener('mouseenter', function () {
+        const value = parseInt(this.getAttribute('data-value'));
+        stars.forEach((s, index) => {
+            if (index < value) {
+                s.classList.add('hovered');
+            } else {
+                s.classList.remove('hovered');
+            }
+        });
+    });
+
+    star.addEventListener('mouseleave', function () {
+        stars.forEach((s) => {
+            s.classList.remove('hovered');
+        });
+        updateStars();
+    });
+
+    star.addEventListener('click', function () {
+        const value = parseInt(this.getAttribute('data-value'));
+        setRating(value);
+        updateStars();
+    });
+});
+//
